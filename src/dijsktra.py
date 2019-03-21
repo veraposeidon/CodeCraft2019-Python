693cdf9eac15ce31361807a44da08c220e6e6e26
veraposeidon <veraposeidon@gmail.com>
2019-03-21 20:43:54 +0800

    整理注释了 utils.py 和 dijsktra.py

--- src/dijsktra.py
+++ src/dijsktra.py
@@ -1,47 +1,51 @@
 # coding: utf-8
-
-# 有向图graph类的实现
-# 最短路径搜索算法：Dijkstra的实现
-# 参考：http://benalexkeen.com/implementing-djikstras-shortest-path-algorithm-with-python/
-
-import collections
 from collections import defaultdict
 
-# 根据道路信息 生成拓扑(注意双向则应有两条)
-# （线段：首ID,尾ID，长度，道路ID）
-# 地图拓扑
-# 有了这份信息之后，road.txt可以抛弃了。
-def createTopology(roads_dict):
-    topologyDict = collections.defaultdict(lambda: [])
 
-    for item in roads_dict.items():
-        start = item[1]['from']
-        end = item[1]['to']
-        length = item[1]['length']
-        roadid = item[1]['id']
-        channel = item[1]['channel']
-        oneway = {'start': start, 'end': end, 'length': length, 'roadid': roadid, 'channel': channel}
-        topologyDict[start].append(oneway)
-        if item[1]['isDuplex'] == 1:
-            oneway = {'start': end, 'end': start, 'length': length, 'roadid': roadid, 'channel': channel}
-            topologyDict[end].append(oneway)
-    return topologyDict
+def create_topology(roads_dict):
+    """
+    从道路信息，构建拓扑信息
+    :param roads_dict:
+    :return:
+    """
+    topology_dict = defaultdict(lambda: [])
+
+    for item in roads_dict.values():
+        # 正向
+        start = item['from']
+        end = item['to']
+        length = item['length']
+        road_id = item['id']
+        channel = item['channel']
+        way = {'start': start, 'end': end, 'length': length, 'road_id': road_id, 'channel': channel}
+        topology_dict[start].append(way)
+
+        # 反向
+        if item['isDuplex'] == 1:
+            way = {'start': end, 'end': start, 'length': length, 'road_id': road_id, 'channel': channel}
+            topology_dict[end].append(way)
+    return topology_dict
 
 
-# 创建图模型
-def createGraph(topologyDict):
+def create_graph(topology_dict):
+    """
+    从拓扑数据生成有向图
+    :param topology_dict:
+    :return:
+    """
     graph = Graph()
-    # topologyDict为
-    for key, value in topologyDict.items():
-        # value 为 list
+
+    for key, value in topology_dict.items():
         for item in value:
             graph.add_edge(item['start'], item['end'], item['weight'])
 
     return graph
 
 
-# 个人理解，Graph应交由调度中心进行管理，管理他的weight,这样繁忙的时候加大他的weight,避免别人走这条路
-class Graph():
+class Graph:
+    """
+    有向图
+    """
     def __init__(self):
         """
          self.edges is a dict of all possible next nodes
@@ -54,23 +58,19 @@ class Graph():
         self.weights = {}
 
     def add_edge(self, from_node, to_node, weight):
-        # 源作假设所有路径为双向，此处我改为单向
-        # # Note: assumes edges are bi-directional
-        # self.edges[from_node].append(to_node)
-        # self.edges[to_node].append(from_node)
-        # self.weights[(from_node, to_node)] = weight
-        # self.weights[(to_node, from_node)] = weight
-
-        # 注意本应用中，来去道路不同，应视为不同的边和不用的权重
         self.edges[from_node].append(to_node)
-        self.weights[(from_node,to_node)] = weight
+        self.weights[(from_node, to_node)] = weight
 
 
-#
 def dijsktra(graph, initial, end):
-    # shortest paths is a dict of nodes
-    # whose value is a tuple of (previous node, weight)
-
+    """
+    dijsktra 最短路径搜索
+    参考： http://benalexkeen.com/implementing-djikstras-shortest-path-algorithm-with-python/
+    :param graph:
+    :param initial:
+    :param end:
+    :return:    shortest paths is a dict of nodes whose value is a tuple of (previous node, weight)
+    """
     shortest_paths = {initial: (None, 0)}
     current_node = initial
     visited = set()
@@ -107,30 +107,29 @@ def dijsktra(graph, initial, end):
     return path
 
 
+def dijsktra_test():
+    graph = Graph()
 
-# def unitTest():
-#     graph = Graph()
-#
-#     edges = [
-#         ('X', 'A', 7),
-#         ('X', 'B', 2),
-#         ('X', 'C', 3),
-#         ('X', 'E', 4),
-#         ('A', 'B', 3),
-#         ('A', 'D', 4),
-#         ('B', 'D', 4),
-#         ('B', 'H', 5),
-#         ('C', 'L', 2),
-#         ('D', 'F', 1),
-#         ('F', 'H', 3),
-#         ('G', 'H', 2),
-#         ('G', 'Y', 2),
-#         ('I', 'J', 6),
-#         ('I', 'K', 4),
-#         ('I', 'L', 4),
-#         ('J', 'L', 1),
-#         ('K', 'Y', 5),
-#     ]
-#
-#     for edge in edges:
-#         graph.add_edge(*edge)
+    edges = [
+        ('X', 'A', 7),
+        ('X', 'B', 2),
+        ('X', 'C', 3),
+        ('X', 'E', 4),
+        ('A', 'B', 3),
+        ('A', 'D', 4),
+        ('B', 'D', 4),
+        ('B', 'H', 5),
+        ('C', 'L', 2),
+        ('D', 'F', 1),
+        ('F', 'H', 3),
+        ('G', 'H', 2),
+        ('G', 'Y', 2),
+        ('I', 'J', 6),
+        ('I', 'K', 4),
+        ('I', 'L', 4),
+        ('J', 'L', 1),
+        ('K', 'Y', 5),
+    ]
+
+    for edge in edges:
+        graph.add_edge(*edge)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,13 @@
 # coding:utf-8
 
 
-# 读取road文件
 def read_road(road_path):
+    """
+    道路信息读取
+    format: (id,length,speed,channel,from,to,isDuplex)
+    :param road_path:
+    :return:
+    """
     roads_dict = dict()
     with open(str(road_path), 'r') as f:
         for line in f.readlines():
@@ -21,8 +26,13 @@ def read_road(road_path):
     return roads_dict
 
 
-# 读取cross文件
 def read_cross(cross_path):
+    """
+    路口信息读取
+    format: (id,roadId,roadId,roadId,roadId)
+    :param cross_path:
+    :return:
+    """
     crosses_dict = dict()
     with open(str(cross_path), 'r') as f:
         for line in f.readlines():
@@ -39,8 +49,13 @@ def read_cross(cross_path):
     return crosses_dict
 
 
-# 读取car文件
 def read_car(car_path):
+    """
+    车辆信息读取
+    format: (id,from,to,speed,planTime)
+    :param car_path:
+    :return:
+    """
     cars_dict = dict()
     with open(str(car_path), 'r') as f:
         for line in f.readlines():
@@ -56,32 +71,36 @@ def read_car(car_path):
             cars_dict[car['id']] = car
     return cars_dict
 
-# networkx 工具箱
-def networkx(topologyDict, plot=False):
+
+def networkx(topology_dict, plot=False):
+    """
+    networkx 工具箱
+    :param topology_dict:
+    :param plot:
+    :return:
+    """
     import networkx as nx
-    G = nx.DiGraph()
+    g = nx.DiGraph()
 
     # 节点
-    for node in topologyDict.keys():
-        G.add_node(node)
+    for node in topology_dict.keys():
+        g.add_node(node)
 
     # 边
     edge_label = dict()
-    for item in topologyDict.items():
+    for item in topology_dict.items():
         for edge in item[1]:
-            G.add_edge(edge['start'], edge['end'], length=edge['length'], weight = edge['length'])
-            edge_label[(edge['start'], edge['end'])] = str(edge['roadid']) + ":" + str(edge['length'])
+            g.add_edge(edge['start'], edge['end'], length=edge['length'], weight=edge['length'])
+            edge_label[(edge['start'], edge['end'])] = str(edge['road_id']) + ":" + str(edge['length'])
 
-    print(G.number_of_nodes())
-    print(G.number_of_edges())
+    print(g.number_of_nodes())
+    print(g.number_of_edges())
 
     if plot:
         import matplotlib.pyplot as plt
-        # G = nx.petersen_graph()
-        # plt.subplot(121)
-        pos = nx.spring_layout(G, weight='length')
-        nx.draw(G, pos, with_labels=True, edge_color='blue', font_weight='bold', node_size=200, node_color='pink')
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_label)
+        pos = nx.spring_layout(g, weight='length')
+        nx.draw(g, pos, with_labels=True, edge_color='blue', font_weight='bold', node_size=200, node_color='pink')
+        nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_label)
         plt.show()
 
-    return G
+    return g
